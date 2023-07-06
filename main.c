57e35eb046904f5ea0e49f6434f83343b8cb9d4b
Bryan Minner <brminner@a1-r4-p5.s19.be>
2023-07-06 15:56:32 +0200

    finish at anvers

--- main.c
+++ main.c
@@ -6,11 +6,16 @@
 /*   By: brminner <brminner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 10:49:06 by brminner          #+#    #+#             */
-/*   Updated: 2023/07/06 12:39:05 by brminner         ###   ########.fr       */
+/*   Updated: 2023/07/06 14:56:55 by brminner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	ft_init(t_philo *philo)
+{
+	
+}
 
 int main(int argc, char **argv)
 {
@@ -19,5 +24,7 @@ int main(int argc, char **argv)
 	if (ft_error_handling(argc, argv) == 0)
 		return (0);
 	if (ft_parsing(argc, argv, &philo) == 0)
+		return (0);
+	if (ft_init(&philo) == 0)
 		return (0);
 }
